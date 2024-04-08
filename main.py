bedcee0cfa7d675ccf967fe680c0fa8a3192e3d5
Joshua <45546025+lejshi3@users.noreply.github.com>
2024-04-08 01:26:55 -0500

    MORE

--- main.py
+++ main.py
@@ -16,6 +16,7 @@ from anthropic import Anthropic
 from pocketbase import PocketBase
 from discord import Client as Discord
 from dotenv import load_dotenv
+from pprint import pprint
 
 # Loading environment variables
 def load_env_vars():
@@ -65,7 +66,8 @@ collections = {
   'nations': pb.collection('nations'),
   'tags': pb.collection('tags'),
   "resources": pb.collection("resources"),
-  "batches": pb.collection("batches")
+  "batches": pb.collection("batches"),
+  "trades": pb.collection("trades")
 }
 
 ai_models = {
@@ -81,25 +83,83 @@ nations = collections["nations"]
 tags = collections["tags"]
 resources = collections["resources"]
 batches = collections["batches"]
+trades = collections["trades"]
 
-resources = {}
 batch_dict = {}
 
-for res_batch in batches.get_full_list(200, {
-  'sort': 'owner',
-  'expand': 'owner, resource',
-  'filter': 'owner.id = "tdzm2lsrfyxlg9n"'
-}
-):
-  batch_owner = res_batch.expand['owner'].name
-  batch_resource = res_batch.expand['resource'].name
-  batch_qty = res_batch.quantity
+def group_batches():
+  for res_batch in batches.get_full_list(200, {
+    'sort': 'owner',
+    'expand': 'owner, resource',
+  }):
+    batch_id = res_batch.id
+    batch_owner = res_batch.expand['owner'].name
+    batch_resource = res_batch.expand['resource'].name
+    batch_qty = res_batch.quantity
 
-  key = batch_resource
-  
-  if key not in batch_dict:
-    batch_dict[key] = 
-  
+    key = (batch_owner, batch_resource)
+    
+    if key not in batch_dict:
+      batch_dict[key] = {
+        'batch_ids': [batch_id],
+        'quantity': batch_qty
+      }
 
-print(resource_list)
-print(batch_list)
+    else:
+      batch_dict[key]['batch_ids'].append(batch_id)
+      batch_dict[key]['quantity'] += batch_qty
+
+  for key, value in batch_dict.items():
+    prime_id = value['batch_ids'][0]
+    total_qty = value['quantity']
+    
+    batches.update(prime_id, {'quantity': total_qty})
+
+    for extra_id in value['batch_ids'][1:]:
+      batches.delete(extra_id) 
+
+# Use `'filter': 'id = "tdzm2lsrfyxlg9n"'` to filter to just Solia.
+
+def check_trades():
+  for trade in trades.get_full_list(200, {
+    'sort': 'name',
+    'filter': 'isActive = True',
+    'expand': 'partyA, partyB'
+  }):
+    name = trade.name
+    partyA = trade.expand['partyA'].id
+    partyA_name = trade.expand['partyA'].name
+    partyB = trade.expand['partyB'].id
+    partyB_name = trade.expand['partyB'].name
+    print(f'{name}: {partyA_name} & {partyB_name}')
+
+
+def monthly_prod():
+  for nation in nations.get_full_list(200, {
+      'sort': 'name'
+    }):
+    nation_id = nation.id
+    base_prod = nation.base_production
+    prod_mod = nation.production_modifiers
+    prod = base_prod
+
+    for resource, value in prod_mod.items():
+      if resource in prod:
+        prod[resource] += value
+
+      else:
+        prod[resource] = value
+      
+    for resource, value in prod.items():
+      current_resource = resources.get_first_list_item(
+        f'name = "{resource}"'
+      )
+      
+      batches.create({
+        'owner': nation_id,
+        'resource': current_resource.id,
+        'quantity': value
+        })
+    
+check_trades()
+group_batches()
